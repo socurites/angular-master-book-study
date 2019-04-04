2c68105a840b582bd80214e9ea82973c84b2dcb9
socurites <socurites@wise-fashion.com>
2019-04-04 22:03:24 +0900

    Add inventory app

---
+++ inventory-app/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {Product} from "./product.model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  product: Product;
+
+  constructor() {
+    let newProduct = new Product(
+      'NICEHAT',
+      'A Nice Black Hat',
+      '/resources/images/products/black-hat.jpg',
+      ['Men', 'Accessories', 'Hats'],
+      29.99
+    );
+
+    this.product = newProduct;
+  }
+}
